6bf0772ab8a5bc5783bef37f433176e6d664c07f
kvarlamov <k.varlamov88@yandex.ru>
2020-08-15 19:17:11 +0300

    Added string extencion to p01

--- p01_Operators/Program.cs
+++ p01_Operators/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace p01_Operators
 {
@@ -9,6 +10,10 @@ namespace p01_Operators
             Point p1 = new Point(1, 2);
             Point p2 = new Point(3, 2);
             var p3 = p1 + p2;
+            Console.WriteLine(p3);
+            string str = "ABC";
+            Console.WriteLine(str.RevertString());
+
             Console.ReadKey();
         }
     }
@@ -23,5 +28,14 @@ namespace p01_Operators
         }
         
         public static Point operator +(Point p1, Point p2) => new Point(p1.X + p2.X, p1.Y + p2.Y);
+        public override string ToString()
+        {
+            return $"[{X};{Y}]";
+        }
+    }
+
+    public static class StringExtencion
+    {
+        public static string RevertString(this string text) => new string(text.ToCharArray().Reverse().ToArray());
     }
 }
