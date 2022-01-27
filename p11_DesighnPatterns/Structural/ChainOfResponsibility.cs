d3aaab6fd2d232a553599f4169b3d3da3015ed3f
kvarlamov <k.varlamov88@yandex.ru>
2022-01-27 23:39:25 +0300

    Continue with patterns

---
+++ p11_DesighnPatterns/Structural/ChainOfResponsibility.cs
@@ -0,0 +1,51 @@
+﻿namespace p11_DesighnPatterns.Structural
+{
+    public class ChainOfResponsibility
+    {
+        public static void Test()
+        {
+            //todo: example isn't completed
+        }
+    }
+
+    public class OrderingSystem
+    {
+        private string _data;
+        
+        public string StartOrdering(string data)
+        {
+            if (Authorize(data))
+            {
+                data = Sanitaize(data);
+                if (FilterIps(data))
+                {
+                    if (data.Equals(_data))
+                    {
+                        return _data;
+                    }
+                    else
+                    {
+                        return data;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private bool FilterIps(string data)
+        {
+            return data.Contains("badIp");
+        }
+
+        private bool Authorize(string data)
+        {
+            return data.Contains("authorized");
+        }
+        
+        public string Sanitaize(string data)
+        {
+            return $"{data} sanitazed";
+        }
+    }
+}
